3867f32b71224117b03a635203b3185a9c5a9bf7
kperegudova <kperegudova@kode-t.ru>
2022-01-28 14:22:24 +0700

    extract data to main thread

--- ToDo/ViewModel/ContentViewModel.swift
+++ ToDo/ViewModel/ContentViewModel.swift
@@ -5,21 +5,31 @@
 
 import UIKit
 
+@MainActor
 class ContentViewModel: ObservableObject {
-  @Published var tasks = [TaskModel]()
-  @Published var priorities = [Priority]()
-  @Published var categories = [Category]()
+  @Published private(set) var tasks = [TaskModel]()
+  @Published private(set) var priorities = [Priority]()
+  @Published private(set) var categories = [Category]()
   
   private let defaults = UserDefaults.standard
   private let baseURL = "http://practice.mobile.kreosoft.ru/api"
   
   func load() async {
-    await getPriorities()
-    await getCategories()
-    await getTasks()
+    await login()
+    do {
+      self.priorities = try await getPriorities()
+      self.categories = try await getCategories()
+      self.tasks = try await getTasks()
+    } catch (let error) {
+      print(error.localizedDescription)
+    }
   }
-  
-  func login(email: String = "tester@gmail.com", password: String = "qwerty1111") async {
+}
+
+
+// TODO: Extract to network service
+extension ContentViewModel {
+  private func login(email: String = "tester@gmail.com", password: String = "qwerty1111") async {
     guard let url = URL(string: "\(baseURL)/login?email=\(email)&password=\(password)") else { return }
     do {
       let (data, _) = try await URLSession.shared.data(from: url)
@@ -30,37 +40,41 @@ class ContentViewModel: ObservableObject {
     }
   }
   
-  private func getPriorities() async {
+  private func getPriorities() async throws -> [Priority] {
     guard let token = defaults.value(forKey: "api_token") as? String,
-          let url = URL(string: "\(baseURL)/priorities?api_token=\(token)") else { return }
+          let url = URL(string: "\(baseURL)/priorities?api_token=\(token)") else { throw CustomError.invalidURL }
     
     do {
       let (data, _) = try await URLSession.shared.data(from: url)
-      priorities = try JSONDecoder().decode([Priority].self, from: data)
+      return try JSONDecoder().decode([Priority].self, from: data)
     } catch (let error) {
-      print("error: ", error.localizedDescription)
+      throw error
     }
   }
   
-  private func getCategories() async {
+  private func getCategories() async throws -> [Category] {
     guard let token = defaults.value(forKey: "api_token") as? String,
-          let url = URL(string: "\(baseURL)/categories?api_token=\(token)") else { return }
+          let url = URL(string: "\(baseURL)/categories?api_token=\(token)") else { throw CustomError.invalidURL }
     do {
       let (data, _) = try await URLSession.shared.data(from: url)
-      categories = try JSONDecoder().decode([Category].self, from: data)
+      return try JSONDecoder().decode([Category].self, from: data)
     } catch (let error) {
-      print("error: ", error.localizedDescription)
+      throw error
     }
   }
   
-  private func getTasks() async {
+  private func getTasks() async throws -> [TaskModel] {
     guard let token = defaults.value(forKey: "api_token") as? String,
-          let url = URL(string: "\(baseURL)/tasks?api_token=\(token)") else { return }
+          let url = URL(string: "\(baseURL)/tasks?api_token=\(token)") else { throw CustomError.invalidURL }
     do {
       let (data, _) = try await URLSession.shared.data(from: url)
-      tasks = try JSONDecoder().decode([TaskModel].self, from: data)
+      return try JSONDecoder().decode([TaskModel].self, from: data)
     } catch (let error) {
-      print("error: ", error.localizedDescription)
+      throw error
     }
   }
+}
+
+enum CustomError: Error {
+  case invalidURL
 }
